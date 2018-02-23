368bb7add137571bad6f7a516bf8e87f05908564
AlexG <alexgreau@live.fr>
2018-02-23 21:24:05 +0100

    Debug nom a une seule partie

--- analyseurWeb/src/Analyseur.java
+++ analyseurWeb/src/Analyseur.java
@@ -8,26 +8,26 @@ public class Analyseur {
         int nomsRep = 0;
         int prenomsRep = 0;
         int fullRep = 0;
-        String title = "Alessandro_Papetti.txt";
+        int totalSpotted = 0;
+        String nom,prenom;
+        int zeub = 0;
+
+        String title = "Kyoto.txt";
+
         // deduire du titre le nom de l'entite
-        String []entities = title.split("_"); // deduire du titre le nom de l'entite
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(entities[0]);
+        String [] entities = title.split("_"); // deduire du titre le nom de l'entite
         //prenom & nom separes
-        String prenom = stringBuilder.toString();
-        stringBuilder.append(" ");
-        String nom = entities[1].split("\\.")[0];
-        stringBuilder.append(nom);
+        prenom = entities[0];
+        nom = entities[entities.length - 1].split("\\.")[0];
 
         Scanner sc = new Scanner(new FileReader(title));
-
         while (sc.hasNext())
         {
 
             String s=sc.next();
             if(s.matches(prenom)){
                 String next = sc.next();
-                if (next.matches(nom+ ".?")){
+                if (next.matches(".?" + nom + ".?")){
                     //System.out.println(s + " " + next + " repere");
                     fullRep ++;
                 }
@@ -36,12 +36,15 @@ public class Analyseur {
                     prenomsRep ++;
                 }
             }
-            else if (s.matches(nom + ".?")) {
+            else if (s.matches(".?" + nom + ".?")) {
                // System.out.println(s + " repere");
                 nomsRep ++;
+                zeub ++;
+
          }
         }
         //print stats
-        System.out.println(" nomsRep = " + nomsRep + "\n prenomsRep = " + prenomsRep + "\n full Rep = " + fullRep);
+        totalSpotted = nomsRep + prenomsRep + fullRep;
+        System.out.println(" nomsRep = " + nomsRep + "\n prenoms = " + prenomsRep + "\n fullname = " + fullRep + "\n \n TOTAL SPOTTED : " + totalSpotted);
     }
 }
