133f85e454632c307a160ab21abf4f50380a59d3
AlexG <alexgreau@live.fr>
2018-02-23 17:04:43 +0100

    Debug reperage nom
    debug : charac spe a la fin des noms : ex "papetti."

--- analyseurWeb/src/Analyseur.java
+++ analyseurWeb/src/Analyseur.java
@@ -5,35 +5,43 @@ import java.util.Scanner;
 public class Analyseur {
     public static  void main(String [] args) throws IOException
     {
+        int nomsRep = 0;
+        int prenomsRep = 0;
+        int fullRep = 0;
         String title = "Alessandro_Papetti.txt";
         // deduire du titre le nom de l'entite
         String []entities = title.split("_"); // deduire du titre le nom de l'entite
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(entities[0]);
-        String entity1 = stringBuilder.toString();
+        //prenom & nom separes
+        String prenom = stringBuilder.toString();
         stringBuilder.append(" ");
-
-        String entity2= entities[1].split("\\.")[0];
-        stringBuilder.append(entity2);
+        String nom = entities[1].split("\\.")[0];
+        stringBuilder.append(nom);
 
         Scanner sc = new Scanner(new FileReader(title));
 
         while (sc.hasNext())
         {
+
             String s=sc.next();
-            if(s.matches(entity1)){
+            if(s.matches(prenom)){
                 String next = sc.next();
-                if (next.matches(entity2)){
-                    System.out.println(s + " " + next + " repere");
+                if (next.matches(nom+ ".?")){
+                    //System.out.println(s + " " + next + " repere");
+                    fullRep ++;
                 }
                 else {
-                    System.out.println(s + " repere");
+                   // System.out.println(s + " repere");
+                    prenomsRep ++;
                 }
             }
-            else if (s.matches(entity2)) {
-                System.out.println(s + " repere");
+            else if (s.matches(nom + ".?")) {
+               // System.out.println(s + " repere");
+                nomsRep ++;
          }
         }
-
+        //print stats
+        System.out.println(" nomsRep = " + nomsRep + "\n prenomsRep = " + prenomsRep + "\n full Rep = " + fullRep);
     }
 }
