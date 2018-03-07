25efc0c527b03dae70481662d74ba5860e92e6d6
AlexG <alexgreau@live.fr>
2018-03-07 01:18:48 +0100

    Debut recherche Pronoms
    tjrs bug de detection en debut de ligne

--- analyseurWeb/src/Analyseur.java
+++ analyseurWeb/src/Analyseur.java
@@ -22,12 +22,34 @@ public class Analyseur {
         return "null";
     }
 
-    public static String[] baliseCreator(String linkName){
+    public static String baliseCreator(String linkName, String corpse ){
         // <entity name=”http://en.wikipedia.org/wiki/Catherine_Deneuve”>Catherine Deneuve</entity>
-        String [] stringTab = new String[2];
+ /*       String [] stringTab = new String[2];
         stringTab[0] = "<entity name=”" + linkName + "”>";
         stringTab[1] = "</entity>";
-        return stringTab;
+*/
+        StringBuffer buff = new StringBuffer();
+        buff.append("<entity name=”");
+        buff.append(linkName);
+        buff.append("”>");
+        buff.append(corpse);
+        buff.append("</entity>");
+
+        return buff.toString();
+    }
+
+    public static boolean deterSexe(String nomTexte){
+        Scanner sc  = new Scanner(nomTexte);
+        // she = true; he = false
+        while (sc.hasNext()){
+            String next = sc.next();
+            if (next.matches("[Ss]he")){
+                return true;
+            }else if (next.matches("[Hh]e")){
+                return false;
+            }
+        }
+        return false;
     }
 
     public static  void main(String [] args) throws IOException
@@ -35,30 +57,33 @@ public class Analyseur {
         int nomsRep = 0;
         int prenomsRep = 0;
         int fullRep = 0;
+        int pronomsRep = 0;
         String nom,prenom, completeTitle;
         String title = "Alessandro_Papetti.txt";
-        String []balise;
-        String debutBalise,finBalise;
+        String link;
 
         // deduire du titre le nom de l'entite
         String [] entities = title.split("_"); // deduire du titre le nom de l'entite
         //prenom & nom separes
         prenom = entities[0];
         nom = entities[entities.length - 1].split("\\.")[0];
+        boolean sexe = deterSexe(title);
+
         completeTitle = title.split("\\.")[0];
 
         // recherche du lien dans les donnees
         System.out.println(linkFinder(completeTitle));
 
         // si lien existe :  creer l'entete de la balise et la fin, pour ensuite les ajouter lors de l'ecriture
-        balise = baliseCreator(linkFinder(completeTitle));
-        debutBalise = balise[0];
-        finBalise = balise [1];
+        link = linkFinder(completeTitle);
+
 
         Scanner sc = new Scanner(new FileReader(title)).useDelimiter("\n");
         File ff= new File("resultat.txt");
         ff.createNewFile();
-        FileWriter fileWriter=new FileWriter(ff);
+        FileWriter fileWriter = new FileWriter(ff);
+        // TODO :  exception du premier grand nom ex : david jean bailey
+
         // TODO : detectiondes pronoms personnels, Date etc et modification de la balise en consequence
 
         while (sc.hasNext()) {
@@ -74,22 +99,23 @@ public class Analyseur {
                     if (next.matches(".?" + nom + ".?")) {
                         // prenom + nom
                         fullRep++;
-                        fileWriter.write(debutBalise);
-                        fileWriter.write(s2 + " " + next);
-                        fileWriter.write(finBalise + " ");
+                        fileWriter.write(baliseCreator(link,s2 + " " + next));
                     } else {
                         // prenom simple
                         prenomsRep++;
-                        fileWriter.write(debutBalise);
-                        fileWriter.write(s2);
-                        fileWriter.write(finBalise + " ");
+                        fileWriter.write(baliseCreator(link, s2));
+
                     }
                 } else if (s2.matches(".?" + nom + ".?")) {
                     // nom repere
                     nomsRep++;
-                    fileWriter.write(debutBalise);
-                    fileWriter.write(s2);
-                    fileWriter.write(finBalise + " ");
+                    fileWriter.write(baliseCreator(link, s2));
+                }else if (sexe = true && s2.matches(".?She") ||s2.matches("she")) {
+                    fileWriter.write(baliseCreator(link, s2));
+                    pronomsRep++;
+                }else if (sexe = false && s2.matches(".?He") ||s2.matches("he")){
+                    fileWriter.write(baliseCreator(link, s2));
+                    pronomsRep++;
                 } else {
                     fileWriter.write(s2 + " ");
                 }
@@ -100,6 +126,6 @@ public class Analyseur {
         //print stats
         int totalSpotted = nomsRep + prenomsRep + fullRep;
 
-        System.out.println(" nomsRep = " + nomsRep + "\n prenoms = " + prenomsRep + "\n fullname = " + fullRep + "\n \n TOTAL SPOTTED : " + totalSpotted);
+        System.out.println(" nomsRep = " + nomsRep + "\n prenoms = " + prenomsRep + "\n fullname = " + fullRep + "\npronoms = "+ pronomsRep+"\n \n TOTAL SPOTTED : " + totalSpotted);
     }
 }
